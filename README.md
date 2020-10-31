001ed54dd616eca74ec05bc67ca5d1b287cfeeee
Jason Nguyen <jason-nguyen@pluralsight.com>
2020-10-30 18:14:14 -0600

    added tests to determine if npgsql was ignoring timeouts

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # npgsl-timeouts-ignored
 An experiment to determine where a PostgreSQL timeout is being ignored
 
+## Is the problem `Npgsql`?
+It depends, `Npgsql` does not respect a `CommandTimeout` at the connection level when using async methods.
+
+Here is how to prove this (note: based on executing in Linux):
+
+- `docker-compose up`
+- `cd Tests`
+- `dotnet test`
+
+There are 4 tests with these results:
+| async | timeout level | timeout respected |
+| --- | --- | --- |
+| False | Connection | True |
+| True | Connection | False |
+| False | Statement | True |
+| True | Statement | True |
+
 ## Is the problem `PostgreSQL`?
 No, the problem is not `PostgreSQL`.  
 Here is how to prove this:
---
+++ Tests/Tests.cs
@@ -0,0 +1,130 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestClass]
+        public class AreConnectionLevelCommandTimeoutsRespected
+        {
+            [TestMethod]
+            public void ForSynchronousMethods()
+            {
+                NpgsqlException thrownException = null;
+
+                try
+                {
+                    var connStr = "Host=localhost;Port=5432;Username=postgres;Password=postgres;CommandTimeout=1;";
+
+                    using var conn = new NpgsqlConnection(connStr);
+                    conn.Open();
+
+                    using (var cmd = new NpgsqlCommand("select pg_sleep(3)::TEXT;", conn))
+                    using (var reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                            Console.WriteLine(reader.GetString(0));
+                }
+                catch (NpgsqlException ex)
+                {
+                    thrownException = ex;
+                }
+
+                Assert.IsNotNull(thrownException, $"Expected a {nameof(NpgsqlException)} to be thrown");
+                Assert.IsTrue(thrownException.InnerException.Message.Contains("Connection timed out"));
+            }
+
+            [TestMethod]
+            public async Task ForAsynchronousMethods()
+            {
+                Exception thrownException = null;
+
+                try
+                {
+                    var connStr = "Host=localhost;Port=5432;Username=postgres;Password=postgres;CommandTimeout=1;";
+
+                    await using var conn = new NpgsqlConnection(connStr);
+                    await conn.OpenAsync();
+
+                    await using (var cmd = new NpgsqlCommand("select pg_sleep(3)::TEXT;", conn))
+                    await using (var reader = await cmd.ExecuteReaderAsync())
+                        while (await reader.ReadAsync())
+                            Console.WriteLine(reader.GetString(0));
+                }
+                catch (Exception ex)
+                {
+                    thrownException = ex;
+                }
+
+                Assert.IsNotNull(thrownException, $"Expected an excpetion to be thrown");
+            }
+        }
+
+        [TestClass]
+        public class AreStatementTimeoutsRespected
+        {
+            [TestMethod]
+            public void ForSynchronousMethods()
+            {
+                PostgresException thrownException = null;
+
+                try
+                {
+                    var connStr = "Host=localhost;Port=5432;Username=postgres;Password=postgres";
+
+                    using var conn = new NpgsqlConnection(connStr);
+                    conn.Open();
+
+                    using (var cmd = new NpgsqlCommand("set statement_timeout to 1000;", conn))
+                        cmd.ExecuteNonQuery();
+
+                    using (var cmd = new NpgsqlCommand("select pg_sleep(3)::TEXT;", conn))
+                    using (var reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                            Console.WriteLine(reader.GetString(0));
+                }
+                catch (PostgresException ex)
+                {
+                    thrownException = ex;
+                }
+
+                Assert.IsNotNull(thrownException, $"Expected a {nameof(PostgresException)} to be thrown");
+                Assert.AreEqual("57014", thrownException?.SqlState);
+                Assert.AreEqual("canceling statement due to statement timeout", thrownException.MessageText);
+            }
+
+            [TestMethod]
+            public async Task ForAsynchronousMethods()
+            {
+                PostgresException thrownException = null;
+
+                try
+                {
+                    var connStr = "Host=localhost;Port=5432;Username=postgres;Password=postgres";
+
+                    await using var conn = new NpgsqlConnection(connStr);
+                    await conn.OpenAsync();
+
+                    await using (var cmd = new NpgsqlCommand("set statement_timeout to 1000;", conn))
+                        await cmd.ExecuteNonQueryAsync();
+
+                    await using (var cmd = new NpgsqlCommand("select pg_sleep(3)::TEXT;", conn))
+                    await using (var reader = await cmd.ExecuteReaderAsync())
+                        while (await reader.ReadAsync())
+                            Console.WriteLine(reader.GetString(0));
+                }
+                catch (PostgresException ex)
+                {
+                    thrownException = ex;
+                }
+
+                Assert.IsNotNull(thrownException, $"Expected a {nameof(PostgresException)} to be thrown");
+                Assert.AreEqual("57014", thrownException?.SqlState);
+                Assert.AreEqual("canceling statement due to statement timeout", thrownException.MessageText);
+            }
+        }
+    }
+}
